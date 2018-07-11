11dbb864ebdca00c34c36fc948c2ac93f5d00b42
Basaluan <lbardaless13@unc.edu.pe>
2018-07-10 22:19:25 -0500

    Actualizar

---
+++ page/js/practica_calificada_02.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function CambiarNoticia() {
+    document.getElementById("titulo").innerHTML = "SE Analiza: live streams sobre la recuperacion economica y el impuslo del consumo privado";
+    document.getElementById("recuadro").innerHTML = "SE Analiza";
+    document.getElementById("tiempo").innerHTML = " 26 de junio de 2018";
+    document.getElementById("texto").innerHTML = noticia_oculta.innerHTML;
+    document.getElementById("grande").src = "/src/img/noticias/i2.PNG";
+}
